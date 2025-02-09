53a6d1078cc27f96a00b33adca1cffb767833066
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-02-08 18:49:01 -0600

    update test for new syscall

--- code/tests/cases/test_command.c
+++ code/tests/cases/test_command.c
@@ -23,15 +23,15 @@
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Define the test suite and add test cases
-FOSSIL_TEST_SUITE(c_command_suite);
+FOSSIL_TEST_SUITE(c_syscall_suite);
 
 // Setup function for the test suite
-FOSSIL_SETUP(c_command_suite) {
+FOSSIL_SETUP(c_syscall_suite) {
     // Setup code here
 }
 
 // Teardown function for the test suite
-FOSSIL_TEARDOWN(c_command_suite) {
+FOSSIL_TEARDOWN(c_syscall_suite) {
     // Teardown code here
 }
 
@@ -43,120 +43,40 @@ FOSSIL_TEARDOWN(c_command_suite) {
 // as samples for library usage.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
-FOSSIL_TEST_CASE(c_test_command) {
-    int32_t result;
 
-    // Test valid command
-    result = fossil_sys_command("echo Hello World");
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test invalid command
-    result = fossil_sys_command("invalid_command");
-    ASSUME_NOT_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(c_test_command_exec) {
+    // Test fossil_sys_command_exec
+    int result = fossil_sys_command_exec("echo Hello");
+    ASSUME_ITS_EQUAL_I32(result, 0);
 }
 
-FOSSIL_TEST_CASE(c_test_command_success) {
-    int32_t result;
-
-    // Test valid command
-    result = fossil_sys_command_success("echo Hello World");
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test invalid command
-    result = fossil_sys_command_success("invalid_command");
-    ASSUME_NOT_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(c_test_command_dup) {
+    // Test fossil_sys_command_dup
+    int oldfd = 1; // Standard output
+    int newfd = fossil_sys_command_dup(oldfd);
+    ASSUME_ITS_TRUE(newfd >= 0);
 }
 
-FOSSIL_TEST_CASE(c_test_command_output) {
-    char output[128];
-    int32_t result;
-
-    // Test valid command
-    result = fossil_sys_command_output("echo Hello World", output, sizeof(output));
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-#ifdef _WIN32
-    // On Windows, the newline is "\r\n" instead of "\n"
-    ASSUME_ITS_EQUAL_CSTR("Hello World\n", output);
-#else
-    ASSUME_ITS_EQUAL_CSTR("Hello World\n", output);
-#endif
+FOSSIL_TEST_CASE(c_test_command_uptime) {
+    // Test fossil_sys_command_uptime
+    int64_t uptime = fossil_sys_command_uptime();
+    ASSUME_ITS_TRUE(uptime >= 0);
 }
 
-FOSSIL_TEST_CASE(c_test_command_exists) {
-    int32_t result;
-
-#ifdef _WIN32
-    // Use "where" on Windows
-    result = fossil_sys_command_exists("where echo");
-#else
-    // Use "which" on Unix-like systems
-    result = fossil_sys_command_exists("which echo");
-#endif
-
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test a non-existing command
-#ifdef _WIN32
-    result = fossil_sys_command_exists("where invalid_command");
-#else
-    result = fossil_sys_command_exists("which invalid_command");
-#endif
-    ASSUME_ITS_EQUAL_I32(0, result);
-}
-
-FOSSIL_TEST_CASE(c_test_command_strcat_safe) {
-    char buffer[16];
-
-    // Initialize buffer
-    strcpy(buffer, "Hello");
-
-    // Test appending within buffer limits
-    fossil_sys_command_strcat_safe(buffer, " World", sizeof(buffer));
-    ASSUME_ITS_EQUAL_CSTR("Hello World", buffer);
-
-    // Test appending with truncation
-    fossil_sys_command_strcat_safe(buffer, "12345678901234567890", sizeof(buffer));
-    ASSUME_ITS_EQUAL_CSTR("Hello World1234", buffer);
-}
-
-FOSSIL_TEST_CASE(c_test_command_erase_exists) {
-    int32_t result;
-
-#ifdef _WIN32
-    // Use a different path on Windows, e.g., "C:\\"
-    result = fossil_sys_command_erase_exists("C:\\");
-#else
-    // Use "/tmp" on Unix-like systems
-    result = fossil_sys_command_erase_exists("/tmp");
-#endif
-
-    ASSUME_ITS_EQUAL_I32(1, result);
-
-    // Test non-existing directory
-    result = fossil_sys_command_erase_exists("non_existing_directory");
-    ASSUME_ITS_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(c_test_command_meminfo) {
+    // Test fossil_sys_command_meminfo
+    int64_t meminfo = fossil_sys_command_meminfo();
+    ASSUME_ITS_TRUE(meminfo >= 0);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
-FOSSIL_TEST_GROUP(c_command_tests) {
-#ifdef __unix__
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command);
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command_success);
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command_output);
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command_exists);
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command_strcat_safe);
-    FOSSIL_TEST_ADD(c_command_suite, c_test_command_erase_exists);
-#else
-    // test cases need to be updated to adress the Windows platform
-    FOSSIL_TEST_SKIP(c_test_command, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(c_test_command_success, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(c_test_command_output, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(c_test_command_exists, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(c_test_command_strcat_safe, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(c_test_command_erase_exists, "Test case not supported on Windows");
-#endif
-    FOSSIL_TEST_REGISTER(c_command_suite);
+FOSSIL_TEST_GROUP(c_syscall_tests) {
+    FOSSIL_TEST_ADD(c_syscall_suite, c_test_command_exec);
+    FOSSIL_TEST_ADD(c_syscall_suite, c_test_command_dup);
+    FOSSIL_TEST_ADD(c_syscall_suite, c_test_command_uptime);
+    FOSSIL_TEST_ADD(c_syscall_suite, c_test_command_meminfo);
+
+    FOSSIL_TEST_REGISTER(c_syscall_suite);
 }
--- code/tests/cases/test_command.cpp
+++ code/tests/cases/test_command.cpp
@@ -23,15 +23,15 @@
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
 // Define the test suite and add test cases
-FOSSIL_TEST_SUITE(cpp_command_suite);
+FOSSIL_TEST_SUITE(cpp_syscall_suite);
 
 // Setup function for the test suite
-FOSSIL_SETUP(cpp_command_suite) {
+FOSSIL_SETUP(cpp_syscall_suite) {
     // Setup code here
 }
 
 // Teardown function for the test suite
-FOSSIL_TEARDOWN(cpp_command_suite) {
+FOSSIL_TEARDOWN(cpp_syscall_suite) {
     // Teardown code here
 }
 
@@ -43,120 +43,77 @@ FOSSIL_TEARDOWN(cpp_command_suite) {
 // as samples for library usage.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
-FOSSIL_TEST_CASE(cpp_test_command) {
-    int32_t result;
 
-    // Test valid command
-    result = fossil_sys_command((char *)"echo Hello World");
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test invalid command
-    result = fossil_sys_command((char *)"invalid_command");
-    ASSUME_NOT_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(cpp_test_command_exec) {
+    // Test fossil_sys_command_exec
+    int result = fossil_sys_command_exec("echo Hello");
+    ASSUME_ITS_EQUAL_I32(result, 0);
 }
 
-FOSSIL_TEST_CASE(cpp_test_command_success) {
-    int32_t result;
-
-    // Test valid command
-    result = fossil_sys_command_success((char *)"echo Hello World");
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test invalid command
-    result = fossil_sys_command_success((char *)"invalid_command");
-    ASSUME_NOT_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(cpp_test_command_dup) {
+    // Test fossil_sys_command_dup
+    int oldfd = 1; // Standard output
+    int newfd = fossil_sys_command_dup(oldfd);
+    ASSUME_ITS_TRUE(newfd >= 0);
 }
 
-FOSSIL_TEST_CASE(cpp_test_command_output) {
-    char output[128];
-    int32_t result;
-
-    // Test valid command
-    result = fossil_sys_command_output((char *)"echo Hello World", output, sizeof(output));
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-#ifdef _WIN32
-    // On Windows, the newline is "\r\n" instead of "\n"
-    ASSUME_ITS_EQUAL_CSTR("Hello World\n", output);
-#else
-    ASSUME_ITS_EQUAL_CSTR("Hello World\n", output);
-#endif
+FOSSIL_TEST_CASE(cpp_test_command_uptime) {
+    // Test fossil_sys_command_uptime
+    int64_t uptime = fossil_sys_command_uptime();
+    ASSUME_ITS_TRUE(uptime >= 0);
 }
 
-FOSSIL_TEST_CASE(cpp_test_command_exists) {
-    int32_t result;
-
-#ifdef _WIN32
-    // Use "where" on Windows
-    result = fossil_sys_command_exists((char *)"where echo");
-#else
-    // Use "which" on Unix-like systems
-    result = fossil_sys_command_exists((char *)"which echo");
-#endif
-
-    ASSUME_ITS_EQUAL_I32(0, result);
-
-    // Test a non-existing command
-#ifdef _WIN32
-    result = fossil_sys_command_exists((char *)"where invalid_command");
-#else
-    result = fossil_sys_command_exists((char *)"which invalid_command");
-#endif
-    ASSUME_ITS_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(cpp_test_command_meminfo) {
+    // Test fossil_sys_command_meminfo
+    int64_t meminfo = fossil_sys_command_meminfo();
+    ASSUME_ITS_TRUE(meminfo >= 0);
 }
 
-FOSSIL_TEST_CASE(cpp_test_command_strcat_safe) {
-    char buffer[16];
-
-    // Initialize buffer
-    strcpy(buffer, "Hello");
-
-    // Test appending within buffer limits
-    fossil_sys_command_strcat_safe(buffer, (char *)" World", sizeof(buffer));
-    ASSUME_ITS_EQUAL_CSTR("Hello World", buffer);
-
-    // Test appending with truncation
-    fossil_sys_command_strcat_safe(buffer, (char *)"12345678901234567890", sizeof(buffer));
-    ASSUME_ITS_EQUAL_CSTR("Hello World1234", buffer);
+FOSSIL_TEST_CASE(cpp_test_command_class_exec) {
+    // Test Command::exec with std::string
+    int result = fossil::sys::Command::exec("echo Hello");
+    ASSUME_ITS_EQUAL_I32(result, 0);
 }
 
-FOSSIL_TEST_CASE(cpp_test_command_erase_exists) {
-    int32_t result;
+FOSSIL_TEST_CASE(cpp_test_command_class_exec_cstr) {
+    // Test Command::exec with const char*
+    int result = fossil::sys::Command::exec("echo Hello");
+    ASSUME_ITS_EQUAL_I32(result, 0);
+}
 
-#ifdef _WIN32
-    // Use a different path on Windows, e.g., "C:\\"
-    result = fossil_sys_command_erase_exists((char *)"C:\\");
-#else
-    // Use "/tmp" on Unix-like systems
-    result = fossil_sys_command_erase_exists((char *)"/tmp");
-#endif
+FOSSIL_TEST_CASE(cpp_test_command_class_dup) {
+    // Test Command::dup
+    int oldfd = 1; // Standard output
+    int newfd = fossil::sys::Command::dup(oldfd);
+    ASSUME_ITS_TRUE(newfd >= 0);
+}
 
-    ASSUME_ITS_EQUAL_I32(1, result);
+FOSSIL_TEST_CASE(cpp_test_command_class_uptime) {
+    // Test Command::uptime
+    int64_t uptime = fossil::sys::Command::uptime();
+    ASSUME_ITS_TRUE(uptime >= 0);
+}
 
-    // Test non-existing directory
-    result = fossil_sys_command_erase_exists((char *)"non_existing_directory");
-    ASSUME_ITS_EQUAL_I32(0, result);
+FOSSIL_TEST_CASE(cpp_test_command_class_meminfo) {
+    // Test Command::meminfo
+    int64_t meminfo = fossil::sys::Command::meminfo();
+    ASSUME_ITS_TRUE(meminfo >= 0);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
-FOSSIL_TEST_GROUP(cpp_command_tests) {
-#ifdef __unix__
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command);
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command_success);
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command_output);
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command_exists);
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command_strcat_safe);
-    FOSSIL_TEST_ADD(cpp_command_suite, cpp_test_command_erase_exists);
-#else
-    // test cases need to be updated to adress the Windows platform
-    FOSSIL_TEST_SKIP(cpp_test_command, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(cpp_test_command_success, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(cpp_test_command_output, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(cpp_test_command_exists, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(cpp_test_command_strcat_safe, "Test case not supported on Windows");
-    FOSSIL_TEST_SKIP(cpp_test_command_erase_exists, "Test case not supported on Windows");
-#endif
-    FOSSIL_TEST_REGISTER(cpp_command_suite);
+
+FOSSIL_TEST_GROUP(cpp_syscall_tests) {
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_exec);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_dup);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_uptime);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_meminfo);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_class_exec);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_class_exec_cstr);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_class_dup);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_class_uptime);
+    FOSSIL_TEST_ADD(cpp_syscall_suite, cpp_test_command_class_meminfo);
+
+    FOSSIL_TEST_REGISTER(cpp_syscall_suite);
 }
