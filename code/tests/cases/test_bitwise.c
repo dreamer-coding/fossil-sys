7b13a0d2fb1983a60614ae9dde2bb345abd61ff4
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-03-07 19:08:59 -0800

    Update test_bitwise.c

--- code/tests/cases/test_bitwise.c
+++ code/tests/cases/test_bitwise.c
@@ -33,16 +33,26 @@ FOSSIL_TEARDOWN(c_bitwise_suite) {
 
 FOSSIL_TEST_CASE(c_test_count_ones) {
     // Signed
-    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_count_ones_i8(0b10101010), 4);
-    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_count_ones_i16(0b1100110011001100), 8);
-    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_count_ones_i32(0xFFFFFFFF), 32);
-    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_count_ones_i64(0xF0F0F0F0F0F0F0F0), 32);
+    int8_t test_val_i8 = 0b10101010;
+    int16_t test_val_i16 = 0b1100110011001100;
+    int32_t test_val_i32 = 0xFFFFFFFF;
+    int64_t test_val_i64 = 0xF0F0F0F0F0F0F0F0;
+
+    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_count_ones_i8(test_val_i8), 4);
+    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_count_ones_i16(test_val_i16), 8);
+    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_count_ones_i32(test_val_i32), 32);
+    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_count_ones_i64(test_val_i64), 32);
 
     // Unsigned
-    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_count_ones_u8(0b10101010), 4);
-    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_count_ones_u16(0b1100110011001100), 8);
-    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_count_ones_u32(0xFFFFFFFF), 32);
-    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_count_ones_u64(0xF0F0F0F0F0F0F0F0), 32);
+    uint8_t test_val_u8 = 0b10101010;
+    uint16_t test_val_u16 = 0b1100110011001100;
+    uint32_t test_val_u32 = 0xFFFFFFFF;
+    uint64_t test_val_u64 = 0xF0F0F0F0F0F0F0F0;
+
+    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_count_ones_u8(test_val_u8), 4);
+    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_count_ones_u16(test_val_u16), 8);
+    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_count_ones_u32(test_val_u32), 32);
+    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_count_ones_u64(test_val_u64), 32);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * *
@@ -51,16 +61,26 @@ FOSSIL_TEST_CASE(c_test_count_ones) {
 
 FOSSIL_TEST_CASE(c_test_bitwise_reverse) {
     // Signed
-    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_reverse_i8(0b00001111), 0b11110000);
-    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_reverse_i16(0b0000000011111111), 0b1111111100000000);
-    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_reverse_i32(0x12345678), 0x1E6A2C48); // Expected reversed bit pattern
-    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_reverse_i64(0x8000000000000001), 0x8000000000000001);
+    int8_t test_val_i8 = 0b00001111;
+    int16_t test_val_i16 = 0b0000000011111111;
+    int32_t test_val_i32 = 0x12345678;
+    int64_t test_val_i64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_reverse_i8(test_val_i8), 0b11110000);
+    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_reverse_i16(test_val_i16), 0b1111111100000000);
+    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_reverse_i32(test_val_i32), 0x1E6A2C48);
+    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_reverse_i64(test_val_i64), 0x8000000000000001);
 
     // Unsigned
-    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_reverse_u8(0b00001111), 0b11110000);
-    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_reverse_u16(0b0000000011111111), 0b1111111100000000);
-    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_reverse_u32(0x12345678), 0x1E6A2C48);
-    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_reverse_u64(0x8000000000000001), 0x8000000000000001);
+    uint8_t test_val_u8 = 0b00001111;
+    uint16_t test_val_u16 = 0b0000000011111111;
+    uint32_t test_val_u32 = 0x12345678;
+    uint64_t test_val_u64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_reverse_u8(test_val_u8), 0b11110000);
+    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_reverse_u16(test_val_u16), 0b1111111100000000);
+    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_reverse_u32(test_val_u32), 0x1E6A2C48);
+    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_reverse_u64(test_val_u64), 0x8000000000000001);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * *
@@ -69,30 +89,50 @@ FOSSIL_TEST_CASE(c_test_bitwise_reverse) {
 
 FOSSIL_TEST_CASE(c_test_bitwise_rotate_left) {
     // Signed
-    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_rotate_left_i8(0b10000001, 2), 0b00000110);
-    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_rotate_left_i16(0x8001, 4), 0x0018);
-    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_rotate_left_i32(0x12345678, 8), 0x34567812);
-    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_rotate_left_i64(0x8000000000000001, 16), 0x0000000000001880);
+    int8_t test_val_i8 = 0b10000001;
+    int16_t test_val_i16 = 0x8001;
+    int32_t test_val_i32 = 0x12345678;
+    int64_t test_val_i64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_rotate_left_i8(test_val_i8, 2), 0b00000110);
+    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_rotate_left_i16(test_val_i16, 4), 0x0018);
+    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_rotate_left_i32(test_val_i32, 8), 0x34567812);
+    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_rotate_left_i64(test_val_i64, 16), 0x0000000000001880);
 
     // Unsigned
-    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_rotate_left_u8(0b10000001, 2), 0b00000110);
-    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_rotate_left_u16(0x8001, 4), 0x0018);
-    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_rotate_left_u32(0x12345678, 8), 0x34567812);
-    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_rotate_left_u64(0x8000000000000001, 16), 0x0000000000001880);
+    uint8_t test_val_u8 = 0b10000001;
+    uint16_t test_val_u16 = 0x8001;
+    uint32_t test_val_u32 = 0x12345678;
+    uint64_t test_val_u64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_rotate_left_u8(test_val_u8, 2), 0b00000110);
+    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_rotate_left_u16(test_val_u16, 4), 0x0018);
+    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_rotate_left_u32(test_val_u32, 8), 0x34567812);
+    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_rotate_left_u64(test_val_u64, 16), 0x0000000000001880);
 }
 
 FOSSIL_TEST_CASE(c_test_bitwise_rotate_right) {
     // Signed
-    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_rotate_right_i8(0b10000001, 2), 0b01100000);
-    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_rotate_right_i16(0x8001, 4), 0x1800);
-    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_rotate_right_i32(0x12345678, 8), 0x78123456);
-    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_rotate_right_i64(0x8000000000000001, 16), 0x0001800000000000);
+    int8_t test_val_i8 = 0b10000001;
+    int16_t test_val_i16 = 0x8001;
+    int32_t test_val_i32 = 0x12345678;
+    int64_t test_val_i64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_I8(fossil_sys_bitwise_rotate_right_i8(test_val_i8, 2), 0b01100000);
+    ASSUME_ITS_EQUAL_I16(fossil_sys_bitwise_rotate_right_i16(test_val_i16, 4), 0x1800);
+    ASSUME_ITS_EQUAL_I32(fossil_sys_bitwise_rotate_right_i32(test_val_i32, 8), 0x78123456);
+    ASSUME_ITS_EQUAL_I64(fossil_sys_bitwise_rotate_right_i64(test_val_i64, 16), 0x0001800000000000);
 
     // Unsigned
-    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_rotate_right_u8(0b10000001, 2), 0b01100000);
-    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_rotate_right_u16(0x8001, 4), 0x1800);
-    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_rotate_right_u32(0x12345678, 8), 0x78123456);
-    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_rotate_right_u64(0x8000000000000001, 16), 0x0001800000000000);
+    uint8_t test_val_u8 = 0b10000001;
+    uint16_t test_val_u16 = 0x8001;
+    uint32_t test_val_u32 = 0x12345678;
+    uint64_t test_val_u64 = 0x8000000000000001;
+
+    ASSUME_ITS_EQUAL_U8(fossil_sys_bitwise_rotate_right_u8(test_val_u8, 2), 0b01100000);
+    ASSUME_ITS_EQUAL_U16(fossil_sys_bitwise_rotate_right_u16(test_val_u16, 4), 0x1800);
+    ASSUME_ITS_EQUAL_U32(fossil_sys_bitwise_rotate_right_u32(test_val_u32, 8), 0x78123456);
+    ASSUME_ITS_EQUAL_U64(fossil_sys_bitwise_rotate_right_u64(test_val_u64, 16), 0x0001800000000000);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * *
