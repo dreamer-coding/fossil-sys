3c767aee2a7f33ca6103bfed7e54c546e93a38de
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-04-16 13:07:09 -0500

    cut out C cases from the cpp test suite

--- code/tests/cases/test_joinpath.cpp
+++ code/tests/cases/test_joinpath.cpp
@@ -45,117 +45,6 @@ FOSSIL_TEARDOWN(cpp_bitwise_suite) {
 
 // need to add char assume ASAP
 
-// ** Test fossil_sys_path_separator Function **
-// FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_separator) {
-//     char sep = fossil_sys_path_separator();
-// #if defined(_WIN32)
-//     ASSUME_ITS_EQUAL_CSTR(sep, '\\');
-// #else
-//     ASSUME_ITS_EQUAL_CS(sep, '/');
-// #endif
-// }
-
-// ** Test fossil_sys_path_envvar Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_envvar) {
-    const char *envvar = fossil_sys_path_envvar();
-#if defined(_WIN32)
-    ASSUME_ITS_EQUAL_CSTR(envvar, "Path");
-#else
-    ASSUME_ITS_EQUAL_CSTR(envvar, "PATH");
-#endif
-}
-
-// ** Test fossil_sys_path_join Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_join) {
-    char *joined = fossil_sys_path_join(const_cast<char *>("home"), const_cast<char *>("user"));
-    ASSUME_NOT_CNULL(joined);
-    ASSUME_ITS_EQUAL_CSTR(joined, "home/user");
-    free(joined);
-
-    joined = fossil_sys_path_join(const_cast<char *>("home/"), const_cast<char *>("user"));
-    ASSUME_NOT_CNULL(joined);
-    ASSUME_ITS_EQUAL_CSTR(joined, "home/user");
-    free(joined);
-}
-
-// ** Test fossil_sys_path_joinv Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_joinv) {
-    const char *paths[] = {const_cast<char *>("home"), const_cast<char *>("user"), const_cast<char *>("documents")};
-    char *joined = fossil_sys_path_joinv(3, paths);
-    ASSUME_NOT_CNULL(joined);
-    ASSUME_ITS_EQUAL_CSTR(joined, "home/user/documents");
-    free(joined);
-}
-
-// ** Test fossil_sys_path_normalize Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_normalize) {
-    char *normalized = fossil_sys_path_normalize(const_cast<char *>("home//user///documents"));
-    ASSUME_NOT_CNULL(normalized);
-    ASSUME_ITS_EQUAL_CSTR(normalized, "home/user/documents");
-    free(normalized);
-}
-
-// ** Test fossil_sys_path_basename Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_basename) {
-    char *basename = fossil_sys_path_basename(const_cast<char *>("home/user/documents/file.txt"));
-    ASSUME_NOT_CNULL(basename);
-    ASSUME_ITS_EQUAL_CSTR(basename, "file.txt");
-    free(basename);
-}
-
-// ** Test fossil_sys_path_dirname Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_dirname) {
-    char *dirname = fossil_sys_path_dirname(const_cast<char *>("home/user/documents/file.txt"));
-    ASSUME_NOT_CNULL(dirname);
-    ASSUME_ITS_EQUAL_CSTR(dirname, "home/user/documents");
-    free(dirname);
-}
-
-// ** Test fossil_sys_path_extname Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_extname) {
-    char *extname = fossil_sys_path_extname(const_cast<char *>("file.txt"));
-    ASSUME_NOT_CNULL(extname);
-    ASSUME_ITS_EQUAL_CSTR(extname, ".txt");
-    free(extname);
-}
-
-// ** Test fossil_sys_path_strip_ext Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_strip_ext) {
-    char *stripped = fossil_sys_path_strip_ext(const_cast<char *>("file.txt"));
-    ASSUME_NOT_CNULL(stripped);
-    ASSUME_ITS_EQUAL_CSTR(stripped, "file");
-    free(stripped);
-}
-
-// ** Test fossil_sys_path_isabs Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_isabs) {
-#if defined(_WIN32)
-    ASSUME_ITS_TRUE(fossil_sys_path_isabs(const_cast<char *>("C:\\Windows")));
-    ASSUME_ITS_FALSE(fossil_sys_path_isabs(const_cast<char *>("Windows\\System32")));
-#else
-    ASSUME_ITS_TRUE(fossil_sys_path_isabs(const_cast<char *>("/usr/bin")));
-    ASSUME_ITS_FALSE(fossil_sys_path_isabs(const_cast<char *>("usr/bin")));
-#endif
-}
-
-// ** Test fossil_sys_path_exists Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_exists) {
-    ASSUME_ITS_TRUE(fossil_sys_path_exists(const_cast<char *>("/")));
-    ASSUME_ITS_FALSE(fossil_sys_path_exists(const_cast<char *>("/nonexistent_path")));
-}
-
-// ** Test fossil_sys_path_isdir Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_isdir) {
-    ASSUME_ITS_TRUE(fossil_sys_path_isdir(const_cast<char *>("/")));
-    ASSUME_ITS_FALSE(fossil_sys_path_isdir(const_cast<char *>("/nonexistent_path")));
-}
-
-// ** Test fossil_sys_path_isfile Function **
-FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_isfile) {
-    ASSUME_ITS_FALSE(fossil_sys_path_isfile(const_cast<char *>("/")));
-    ASSUME_ITS_FALSE(fossil_sys_path_isfile(const_cast<char *>("/nonexistent_file")));
-}
-
 // // ** Test fossil::sys::Path::separator Function **
 // FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_separator_class) {
 //     std::string sep = fossil::sys::Path::separator();
@@ -255,20 +144,6 @@ FOSSIL_TEST_CASE(cpp_test_fossil_sys_path_isfile_class) {
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
 FOSSIL_TEST_GROUP(cpp_path_tests) {
-    // FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_separator);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_envvar);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_join);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_joinv);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_normalize);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_basename);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_dirname);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_extname);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_strip_ext);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_isabs);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_exists);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_isdir);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_isfile);
-
     // FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_separator_class);
     FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_envvar_class);
     FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_fossil_sys_path_join_class);
