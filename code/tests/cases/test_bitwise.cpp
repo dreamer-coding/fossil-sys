e552ec0ce20f5e90d23c67af28cc23e65dc5e170
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-04-16 13:10:56 -0500

    cut c cases from another cpp test suite

--- code/tests/cases/test_bitwise.cpp
+++ code/tests/cases/test_bitwise.cpp
@@ -43,70 +43,6 @@ FOSSIL_TEARDOWN(cpp_bitwise_suite) {
 // as samples for library usage.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
-// ** Test fossil_sys_bitwise_parse Function **
-FOSSIL_TEST_CASE(cpp_test_bitwise_parse) {
-    fossil_sys_bitwise_table_t table[] = {
-        {"read", 0x1},
-        {"write", 0x2},
-        {"execute", 0x4},
-        {NULL, 0}
-    };
-
-    uint64_t result = fossil_sys_bitwise_parse("read|write", table);
-    ASSUME_ITS_EQUAL_I32(result, 0x3); // 0x1 | 0x2
-
-    result = fossil_sys_bitwise_parse("execute", table);
-    ASSUME_ITS_EQUAL_I32(result, 0x4);
-
-    result = fossil_sys_bitwise_parse("", table);
-    ASSUME_ITS_EQUAL_I32(result, 0x0);
-}
-
-// ** Test fossil_sys_bitwise_format Function **
-FOSSIL_TEST_CASE(cpp_test_bitwise_format) {
-    fossil_sys_bitwise_table_t table[] = {
-        {"read", 0x1},
-        {"write", 0x2},
-        {"execute", 0x4},
-        {NULL, 0}
-    };
-
-    char buffer[64];
-    int status = fossil_sys_bitwise_format(0x3, table, buffer, sizeof(buffer));
-    ASSUME_ITS_EQUAL_I32(status, 0);
-    ASSUME_ITS_EQUAL_CSTR(buffer, "read|write");
-
-    status = fossil_sys_bitwise_format(0x4, table, buffer, sizeof(buffer));
-    ASSUME_ITS_EQUAL_I32(status, 0);
-    ASSUME_ITS_EQUAL_CSTR(buffer, "execute");
-
-    status = fossil_sys_bitwise_format(0x0, table, buffer, sizeof(buffer));
-    ASSUME_ITS_EQUAL_I32(status, 0);
-    ASSUME_ITS_EQUAL_CSTR(buffer, "");
-}
-
-// ** Test fossil_sys_bitwise_lookup Function **
-FOSSIL_TEST_CASE(cpp_test_bitwise_lookup) {
-    fossil_sys_bitwise_table_t table[] = {
-        {"read", 0x1},
-        {"write", 0x2},
-        {"execute", 0x4},
-        {NULL, 0}
-    };
-
-    uint64_t bit;
-    int status = fossil_sys_bitwise_lookup("read", table, &bit);
-    ASSUME_ITS_EQUAL_I32(status, 0);
-    ASSUME_ITS_EQUAL_I32(bit, 0x1);
-
-    status = fossil_sys_bitwise_lookup("write", table, &bit);
-    ASSUME_ITS_EQUAL_I32(status, 0);
-    ASSUME_ITS_EQUAL_I32(bit, 0x2);
-
-    status = fossil_sys_bitwise_lookup("unknown", table, &bit);
-    ASSUME_NOT_EQUAL_I32(status, 0);
-}
-
 // ** Test fossil::sys::Bitwise::parse Function **
 FOSSIL_TEST_CASE(cpp_test_class_bitwise_parse) {
     fossil_sys_bitwise_table_t table[] = {
@@ -175,10 +111,6 @@ FOSSIL_TEST_CASE(cpp_test_class_bitwise_lookup) {
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
 FOSSIL_TEST_GROUP(cpp_bitwise_tests) {
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_bitwise_parse);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_bitwise_format);
-    FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_bitwise_lookup);
-
     FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_class_bitwise_parse);
     FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_class_bitwise_format);
     FOSSIL_TEST_ADD(cpp_bitwise_suite, cpp_test_class_bitwise_lookup);
